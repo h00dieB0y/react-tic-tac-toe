a3c2f451e589010fa810d9fe68fb10ea6c434bc5
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-04 10:37:09 +0100

    feat: implement GameContext and provider for game state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import GamePage from "./components/pages/GamePage.tsx";
+import { GameProvider } from './contexts/GameContexts.tsx';
 
 function App() {
 
   return (
-    <>
+    <GameProvider>
       <GamePage />
-    </>
+    </GameProvider>
   )
 }
 
---
+++ src/contexts/GameContexts.tsx
@@ -0,0 +1,35 @@
+import React, {createContext, useContext} from "react";
+import useGameEngine from "../hooks/useGameEngine";
+import {Game, Player} from "../types/game";
+
+interface GameContextValue {
+    gameState: Game;
+    winner: Player | null;
+    handleClick: (i: number) => void;
+    startGame: (playerSymbol: Player, gridSize: number, winCondition: number) => void;
+    resetGame: () => void;
+}
+
+interface GameProviderProps {
+    children: React.ReactNode;
+}
+
+const GameContext = createContext<GameContextValue | undefined>(undefined);
+
+export const GameProvider: React.FC<GameProviderProps> = ({children}) => {
+    const {gameState, winner, handleClick, startGame, resetGame} = useGameEngine({gridSize: 3, winCondition: 3});
+
+    return (
+        <GameContext.Provider value={{gameState, winner, handleClick, startGame, resetGame}}>
+            {children}
+        </GameContext.Provider>
+    );
+}
+
+export const useGame = () => {
+    const context = useContext(GameContext);
+    if (!context) {
+        throw new Error("useGame must be used within a GameProvider");
+    }
+    return context;
+}
